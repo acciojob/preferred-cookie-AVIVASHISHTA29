d86893fce4e8401a96b68ff6038dc04dcec79ecd
Avi Vashishta <avivashishta@gmail.com>
2024-01-26 21:46:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+document.getElementById("myForm").addEventListener("submit",(e)=>{
+
+	e.preventDefault();
+	let fontsize = document.getElementById("fontsize").value;
+	let fontcolor = document.getElementById("fontcolor").value;
+
+	document.cookie = `fontsize=${fontsize};`
+	document.cookie = `fontcolor=${fontcolor};`
+})
+
+let cookies = Object.fromEntries(
+	document.cookie.split("; ").
+	map((item)=>item.split("=")));
+
+document.getElementById("fontsize").value = cookies.fontsize??0;
+document.getElementById("fontcolor").value = cookies.fontcolor??"#000";
+
+
